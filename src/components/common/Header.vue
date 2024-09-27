8b6e07e5196d41afa1d2d485caa6b46c77ea7030
Abdul Rahmat <abdulrahmat97dev@gmail.com>
2024-09-27 11:26:34 +0700

    fixing styling header

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col justify-center gap-y-3 bg-white">
     <div class="text-center">
-      <h1 class="text-2xl">Knowledge base doesn't have to be boring</h1>
+      <h1 class="text-2xl font-bold">Knowledge base doesn't have to be boring</h1>
       <h3 class="text-xl">Everything you need to manage your messaging</h3>
     </div>
     <SearchBar />
